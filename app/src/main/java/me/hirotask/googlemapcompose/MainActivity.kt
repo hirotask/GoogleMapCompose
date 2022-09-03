ecfce8f91090e4a42df0102f56b828481fa1ae2b
hirotask <hirotaskhashi@gmail.com>
2022-09-03 13:14:42 +0900

    [update]: GoogleMapsを表示できるように

--- app/src/main/java/me/hirotask/googlemapcompose/MainActivity.kt
+++ app/src/main/java/me/hirotask/googlemapcompose/MainActivity.kt
@@ -6,10 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
 import me.hirotask.googlemapcompose.ui.theme.GoogleMapComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +20,32 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GoogleMapComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    GoogleMaps()
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun GoogleMaps() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GoogleMapComposeTheme {
-        Greeting("Android")
+        val singapore = LatLng(1.35, 103.87)
+        val cameraPositionState = rememberCameraPositionState {
+            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                position = singapore,
+                title = "Singapore",
+                snippet = "Marker in Singapore"
+            )
+        }
     }
 }
