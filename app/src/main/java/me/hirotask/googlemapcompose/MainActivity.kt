7fd15aa39c5d811c53394d12f41d58d21389c76a
hirotask <hirotaskhashi@gmail.com>
2022-09-05 13:09:45 +0900

    [update]: Mapを長押しした地点にマーカーを指すようにした

--- app/src/main/java/me/hirotask/googlemapcompose/MainActivity.kt
+++ app/src/main/java/me/hirotask/googlemapcompose/MainActivity.kt
@@ -3,16 +3,32 @@ package me.hirotask.googlemapcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import com.google.android.gms.maps.model.CameraPosition
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.maps.android.ktx.awaitMap
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import me.hirotask.googlemapcompose.ui.theme.GoogleMapComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,21 +47,66 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
+    @Preview
     fun GoogleMaps() {
+        val mapView = rememberMapViewWithLifeCycle()
+        Box {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.White)
+            ) {
+                AndroidView({ mapView }) { mapView ->
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val map = mapView.awaitMap()
+                        map.uiSettings.isZoomControlsEnabled = true
 
-        val singapore = LatLng(1.35, 103.87)
-        val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(singapore, 10f)
-        }
-        GoogleMap(
-            modifier = Modifier.fillMaxSize(),
-            cameraPositionState = cameraPositionState
-        ) {
-            Marker(
-                position = singapore,
-                title = "Singapore",
-                snippet = "Marker in Singapore"
-            )
+                        map.setOnMapLongClickListener {
+                            map.addMarker(MarkerOptions().title("mark").position(it))
+                        }
+
+                    }
+                }
+            }
         }
     }
+
+    @Composable
+    fun rememberMapLifeCycleObserver(
+        mapView: MapView
+    ): LifecycleEventObserver = remember(mapView) {
+        LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+                Lifecycle.Event.ON_START -> mapView.onStart()
+                Lifecycle.Event.ON_RESUME -> mapView.onResume()
+                Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+                Lifecycle.Event.ON_STOP -> mapView.onStop()
+                Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+                else -> throw IllegalStateException()
+            }
+        }
+    }
+
+    @Composable
+    fun rememberMapViewWithLifeCycle(): MapView {
+        val context = LocalContext.current
+        val mapView = remember {
+            MapView(context).apply {
+                id = R.id.mapFragment
+            }
+        }
+
+        val lifecycleObserver = rememberMapLifeCycleObserver(mapView = mapView)
+        val lifecycle = LocalLifecycleOwner.current.lifecycle
+        DisposableEffect(lifecycle) {
+            lifecycle.addObserver(lifecycleObserver)
+            onDispose {
+                lifecycle.removeObserver(lifecycleObserver)
+            }
+        }
+
+        return mapView
+    }
+
 }
